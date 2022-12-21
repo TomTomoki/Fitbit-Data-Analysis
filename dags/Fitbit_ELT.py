da86183be89af6f5a13fa2792022d3be3f9fd794
Tomoki Kyotani <tkyotani121@gmail.com>
2022-12-21 12:07:21 +0900

    Replace hard-coded dates

--- dags/Fitbit_ELT.py
+++ dags/Fitbit_ELT.py
@@ -20,14 +20,13 @@ def fitbit_taskflow():
     def extract_load():
         fb = Fitbit(config.fitbit_client_id, 
                     config.fitbit_access_token,
-                    '2023-01-01')
+                    config.fitbit_access_token_expires_on)
 
         db_conn = DBConnector(config.db_user, config.db_password, config.db_name, config.db_host)
         db_conn.connect()
 
         types = ['sleep', 'steps', 'calories']
-        records = { t: None for t in types }
-        record_date = '2022-11-16'
+        record_date = pdl.now('America/Los_Angeles').to_date_string()
 
         for type in types:
             res = fb.get_records(type, record_date)
