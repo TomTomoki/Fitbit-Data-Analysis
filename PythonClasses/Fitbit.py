50cfe8b9732284b0d3d4886e52a0e072804b91fc
Tomoki Kyotani <tkyotani121@gmail.com>
2022-12-21 11:44:48 +0900

    Replace datetime with pendulum

--- PythonClasses/Fitbit.py
+++ PythonClasses/Fitbit.py
@@ -1,13 +1,12 @@
 import requests
-from datetime import datetime, timedelta
-import pytz
+import pendulum as pdl
 
 class Fitbit:
     def __init__(self, client_id, access_token, access_token_expires_on):
         self.client_id = client_id
         self.access_token = access_token
         self.headers = {'authorization': 'Bearer ' + self.access_token}
-        self.access_token_expires_on = datetime.strptime(access_token_expires_on, '%Y-%m-%d').date()
+        self.access_token_expires_on = pdl.from_format(access_token_expires_on, 'YYYY-MM-DD').date()
 
     def get_records(self, type, date):
         if self.access_token_expired():
@@ -35,7 +34,6 @@ class Fitbit:
         Returns:
             bool: True if expired
         """
-        pst = pytz.timezone('America/Los_Angeles')
-        now = datetime.now(pst)
+        now = pdl.now('America/Los_Angeles').date()
         
         return now > self.access_token_expires_on
