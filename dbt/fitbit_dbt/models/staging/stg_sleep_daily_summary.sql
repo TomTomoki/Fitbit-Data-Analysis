3b3573d84a5e316ce8e9f648547bacb0f37a8702
Tomoki Kyotani <tkyotani121@gmail.com>
2023-01-17 09:03:58 +0000

    Create a new stg dbt model for Fitbit sleep daily summary

---
+++ dbt/fitbit_dbt/models/staging/stg_sleep_daily_summary.sql
@@ -0,0 +1,28 @@
+with sleep_overview as (
+	select
+		load_json #> '{sleep,0}' ->> 'dateOfSleep' as dateOfSleep
+		, load_json #> '{sleep,0}' ->> 'duration' as duration
+		, load_json #> '{sleep,0}' ->> 'efficiency' as efficiency
+		, load_json #> '{sleep,0}' ->> 'endTime' as endTime
+		, load_json #> '{sleep,0}' ->> 'isMainSleep' as isMainSleep
+		, load_json #> '{sleep,0}' ->> 'logId' as logId
+		, load_json #> '{sleep,0}' ->> 'minutesAfterWakeup' as minutesAfterWakeup
+		, load_json #> '{sleep,0}' ->> 'minutesAsleep' as minutesAsleep
+		, load_json #> '{sleep,0}' ->> 'minutesAwake' as minutesAwake
+		, load_json #> '{sleep,0}' ->> 'minutesToFallAsleep' as minutesToFallAsleep
+		, load_json #> '{sleep,0}' ->> 'startTime' as startTime
+		, load_json #> '{sleep,0}' ->> 'timeInBed' as timeInBed
+		, load_json -> 'summary' -> 'totalMinutesAsleep' as totalMinutesAsleep
+		, load_json -> 'summary' -> 'totalSleepRecords' as totalSleepRecords
+		, load_json -> 'summary' -> 'totalTimeInBed' as totalTimeInBed
+		, load_timestamp
+	from landing.sleep
+	{% if is_incremental() %}
+
+		where load_json #> '{sleep,0}' ->> 'dateOfSleep' > (select COALESCE(max(dateOfSleep), '2022-09-30') from {{ this }})
+
+	{% endif %}
+)
+select
+    *
+from sleep_overview
